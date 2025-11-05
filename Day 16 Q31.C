433dd673f5624628e5eb054f51f0b8d21cf3a160
agrawalrachit414-lab <agrawalrachit414@gmail.com>
2025-11-05 17:07:40 +0530

    Add C program to convert decimal to binary

---
+++ Day 16 Q31.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int binary, i = 0;
+    if(n == 0)
+        printf("Binary: 0");
+    else {
+        while(n > 0) {
+            binary[i++] = n % 2;
+            n /= 2;
+        }
+        printf("Binary: ");
+        for(int j = i - 1; j >= 0; j--)
+            printf("%d", binary[j]);
+    }
+    printf("\n");
+    return 0;
+}
